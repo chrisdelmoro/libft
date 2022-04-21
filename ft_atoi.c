6ed84410b9c8697ea9731141ef3af5824a560082
ccamargo <ccamargo@student.42sp.org.br>
2022-04-21 19:18:17 -0300

    Fix problem with atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: ccamargo <ccamargo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 21:02:35 by ccamargo          #+#    #+#             */
-/*   Updated: 2022/04/10 16:48:28 by ccamargo         ###   ########.fr       */
+/*   Updated: 2022/04/21 19:17:38 by ccamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ static int	ft_isspace(char c)
 int	ft_atoi(const char *nptr)
 {
 	size_t	i;
+	size_t	nbr;
 	int		sign;
-	int		nbr;
 
 	i = 0;
-	sign = 1;
 	nbr = 0;
+	sign = 1;
 	while (ft_isspace(nptr[i]))
 		i++;
 	if (nptr[i] == '-')
@@ -41,5 +41,5 @@ int	ft_atoi(const char *nptr)
 		nbr = (nbr * 10) + nptr[i] - '0';
 		i++;
 	}
-	return (nbr * sign);
+	return ((int)(nbr * sign));
 }
